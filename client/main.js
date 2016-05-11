0ffe1da10b59c67f9a7a16341cd0d4bc4f8e48b2
Luis Filipe <luis.fgbo@gmail.com>
2016-05-11 22:35:38 +0100

    messy stuff

--- client/main.js
+++ client/main.js
@@ -1,22 +1,35 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
+//
+// import './main.html';
+//
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+//
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+//
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.mainLayout.onRendered(function () {
+    console.log('rendered main layout');
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.mainLayout.onDestroyed(function () {
+
+    console.log('destroy')
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.test.onRendered(function () {
+    console.log('rendered test');
 });
---
+++ server/init.js
@@ -0,0 +1,6 @@
+/**
+ * Created by luis on 07/05/16.
+ */
+Meteor.startup(function () {
+
+});
